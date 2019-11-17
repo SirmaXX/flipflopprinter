91ac3347b0c7af9adf348355bf6faa5ba3da23cf
Deniz <sea-97@hotmail.com>
2019-11-18 02:30:38 +0300

    main project completed

--- options.js
+++ options.js
@@ -1,16 +1,20 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 'use strict';
-function myFunction() {
+function getpages() {
 var text = "";
 var i; 
+var frontpages =[];
+var backpages = [];
 var lastnumber = document.getElementById("myText").value;
 for (i = 0; i < lastnumber ; i++) {
   text +=  i + ",";
+  if(i%2 ==0){
+    frontpages.push(i);
+  }else {
+   backpages.push(i);
+  }
 }
 document.getElementById("demo").innerHTML = text;
-
-
+document.getElementById("front").innerHTML = frontpages;
+document.getElementById("back").innerHTML = backpages;
 }
