e333006896c317a48bb8660420e409c9ed3fed51
Deniz <sea-97@hotmail.com>
2019-12-07 23:48:56 +0300

    add new theme ,new properties

--- options.js
+++ options.js
@@ -3,18 +3,91 @@
 function getpages() {
 var text = "";
 var i; 
-var frontpages =[];
-var backpages = [];
-var lastnumber = document.getElementById("myText").value;
-for (i = 0; i < lastnumber ; i++) {
-  text +=  i + ",";
-  if(i%2 ==0){
-    frontpages.push(i);
-  }else {
-   backpages.push(i);
-  }
+const frontpages =[];
+const backpages = [];
+const lastnumber = document.getElementById("myText").value;
+var frontpgs=document.getElementById("front");
+var backpgs=document.getElementById("back");
+var numbers = /^[0-9]+$/;
+
+if(lastnumber.match(numbers))
+      {
+      for (i = 0; i <  lastnumber; i++) {
+        text +=  i + ",";
+        if(i%2 ==0){
+         frontpages.push(i);
+         }else {
+         backpages.push(i);
+         }
+        }
+      }
+      else
+      {
+        alert("not number");
+        reloadfunc();
+      return false;
+      }
+
+ 
+
+  //they are outputs
+
+frontpgs.innerHTML = frontpages.filter(checkpages);
+backpgs.innerHTML = backpages.filter(checkpages);
+copyText(frontpages);
+
 }
-document.getElementById("demo").innerHTML = text;
-document.getElementById("front").innerHTML = frontpages;
-document.getElementById("back").innerHTML = backpages;
+
+function copyText(x) {
+  var frontnumbers=x.slice();
+  var copy = JSON.stringify(frontnumbers)
+  document.execCommand("copy");
+  console.log(copy);
+  alert("Copied the text: " +JSON.stringify(copy));
+}
+
+
+function intervalpages(){
+var text="";
+  var first = document.getElementById("first").value;
+  var last = document.getElementById("last").value;
+  var backpg=[];
+  var frontpg=[];
+  var numbers = /^[0-9]+$/;
+
+  if(first.match(numbers) && last.match(numbers) )
+        {
+        for (first; first < last; first++) {
+          text += first + ",";
+          if(first%2 ==0){
+            frontpg.push(first);
+           }else {
+            backpg.push(first);
+           }
+          }
+        }
+        else
+        {
+          alert("not number");
+          reloadfunc();
+        return false;
+        }
+        document.getElementById("frontpgs").innerHTML = backpg;
+        document.getElementById("backpgs").innerHTML = frontpg;
+}
+
+
+//its checking outputs
+function checkpages(value) {
+  if (typeof value === 'number'){
+    return value;
+    }else {
+     alert("lütfen sayı giriniz");
+    }
+   
+}
+
+//its page reload function
+function reloadfunc() {
+  location.reload();
 }
